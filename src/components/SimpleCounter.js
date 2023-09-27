380f472ccccfec881bdd0b919b8757a2749330d9
Michail Bodzyaev <redekwork@gmail.com>
2023-09-27 11:41:39 +0200

    moing into react

---
+++ src/components/SimpleCounter.js
@@ -0,0 +1,22 @@
+import { useState} from "react";
+
+export const SimpleCounter = function(){
+
+    const [counter, setCounter] = useState(0)
+
+    const updateCounter = function (counterAdjustment){
+        setCounter( (counter) => counter + counterAdjustment > 0 ? counter + counterAdjustment : 0)
+    }   
+    return (
+        <div>
+            <div>{counter}</div>
+            <button onClick={() => updateCounter(10)}>Plus 10</button>
+            <button onClick={() => updateCounter(1)}>Plus</button>
+            <button onClick={() => updateCounter(-Infinity)}>Reset</button>
+            <button onClick={() => updateCounter(-1)}>Minus</button>
+            <button onClick={() => updateCounter(-10)}>Minus 10</button>
+        </div>
+    )
+
+
+}
